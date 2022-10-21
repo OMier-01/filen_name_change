9aab3933fac14bc6a20350ad0e12cf84d2c874f8
Oscar D. Mier (he/him/his) <83384413+OMier-01@users.noreply.github.com>
2022-10-20 22:10:03 -0700

    Added python script

---
+++ File_name_change.py
@@ -0,0 +1,21 @@
+"""
+Name: Oscar D. mier
+Date: 10/20/2022
+Position: Technical Solutions Engineer - Alto Neuroscience 
+
+This code ensures there are no errors in the file nomenclatures for our cognition and EEG data. 
+"""
+# Pseudo Code
+# Navigate to the correct directory
+# Determine how many files are in a folder
+# Create a for i loop in range of the number of files in the folder
+# find file names with repeating sessions
+# Slice the repeated session nomenclature
+# rename file name correctly without the repeated session
+# Have the files saved into a new folder
+
+
+def main()
+"""
+main function to call the other functions 
+"""
